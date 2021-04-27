397007f84e906a46b1851f4a96e627e154f8a5e4
Maninder Singh Bawa <maninderbawavip@gmail.com>
2021-04-27 22:38:59 +0530

    made changes

---
+++ emailconfig.js
@@ -0,0 +1,7 @@
+"DEFAULT_FROM_EMAIL": "hello@ialabs.co.in",
+"EMAIL_BACKEND": "django.core.mail.backends.smtp.EmailBackend",
+"EMAIL_HOST": "smtp.gmail.com",
+"EMAIL_PORT": 587,
+"EMAIL_USE_TLS": true,
+"EMAIL_HOST_PASSWORD": "iAL4j>33R",
+"EMAIL_HOST_USER": "hello@organiq.in"
